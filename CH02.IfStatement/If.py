58d55f9447ccb2a338ac1e760608677b553e2351
serak0310 <serak0310@naver.com>
2022-08-11 13:52:08 +0900

    Ch02. 조건문

---
+++ CH02.IfStatement/If.py
@@ -0,0 +1,15 @@
+# 조건문
+
+x = 15
+if x>=10:
+    print(x)
+
+score = 85
+if score >= 90:
+    print("학점: A")
+elif score >= 80:
+    print("학점: B")
+elif score >= 70:
+    print("학점: c")
+else:
+    print("학점: F")
